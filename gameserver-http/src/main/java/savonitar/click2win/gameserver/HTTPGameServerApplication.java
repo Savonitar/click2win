8c7e38607443651ae27e62a59604bfdb6bacb24e
Savonitar <savonitar@gmail.com>
2024-04-28 18:53:01 +0200

    Add HTTP implementation

---
+++ gameserver-http/src/main/java/savonitar/click2win/gameserver/HTTPGameServerApplication.java
@@ -0,0 +1,11 @@
+package savonitar.click2win.gameserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HTTPGameServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HTTPGameServerApplication.class, args);
+    }
+}
---
+++ gameserver-http/src/main/java/savonitar/click2win/gameserver/configuration/CommonConfiguration.java
@@ -0,0 +1,76 @@
+package savonitar.click2win.gameserver.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+import savonitar.click2win.core.GameSessionProcessor;
+import savonitar.click2win.core.PlayerGameSession;
+import savonitar.click2win.core.gameplay.EventSequenceGenerator;
+import savonitar.click2win.gameserver.MatchStatus;
+import savonitar.click2win.gameserver.protobuf.ServerGameEvent;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Configuration
+public class CommonConfiguration {
+
+    @Bean
+    public ProtobufHttpMessageConverter protobufHttpMessageConverter() {
+        return new ProtobufHttpMessageConverter();
+    }
+
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public RestTemplate restTemplate(ProtobufHttpMessageConverter protobufHttpMessageConverter,
+                                     StringHttpMessageConverter stringHttpMessageConverter) {
+        return new RestTemplate(List.of(protobufHttpMessageConverter, stringHttpMessageConverter));
+    }
+
+    @Bean
+    public Map<PlayerGameSession, MatchStatus> sessionToMatchStatus() {
+        return new ConcurrentHashMap<>();
+    }
+
+    @Bean
+    public Map<PlayerGameSession, ServerGameEvent> sessionToLastTargetGameEvent() {
+        return new ConcurrentHashMap<>();
+    }
+
+    @Bean
+    public Map<PlayerGameSession, AtomicInteger> sessionToPlayerHits() {
+        return new ConcurrentHashMap<>();
+    }
+
+    @Bean
+    public GameSessionProcessor gameSessionProcessor(Map<PlayerGameSession, ServerGameEvent> sessionToLastTargetGameEvent,
+                                                     Map<PlayerGameSession, AtomicInteger> sessionToPlayerHits) {
+        return new GameSessionProcessor(sessionToLastTargetGameEvent, sessionToPlayerHits);
+    }
+
+    @Bean
+    public EventSequenceGenerator eventSequenceGenerator(int gameSessionDurationMs, int eventGenerationIntervalMs) {
+        return new EventSequenceGenerator(gameSessionDurationMs, eventGenerationIntervalMs);
+    }
+
+    @Bean
+    public int gameSessionDurationMs(@Value("${game.session.duration:5000}") int gameSessionDurationMs) {
+        return gameSessionDurationMs;
+    }
+
+    @Bean
+    public int eventGenerationIntervalMs(@Value("${game.event.generation:1000}") int eventGenerationIntervalMs) {
+        return eventGenerationIntervalMs;
+    }
+}
