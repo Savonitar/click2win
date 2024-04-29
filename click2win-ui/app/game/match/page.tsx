3b5cc24fa066e5a03d271f289263abe2fdb4cc1c
Alexander <savonitar@gmail.com>
2024-04-29 21:38:42 +0200

    Update UI for HTTP GameServer Implementation (#16)

--- click2win-ui/app/game/match/page.tsx
+++ click2win-ui/app/game/match/page.tsx
@@ -1,13 +1,24 @@
 "use client";
-import { ServerGameEvent, PlayerClickedEvent } from '@/app/proto/api_pb';
-import { Message, proto3 } from "@bufbuild/protobuf";
 import React, { useState, useEffect, useRef } from 'react';
-import { apiUrl } from "../../api/constants";
+// import { apiUrl } from "../../api/constants";
+type PlayerClickedEvent = {
+  x: number;
+  y: number;
+};
+
+type ServerGameEvent = {
+  x: number;
+  y: number;
+  end: boolean;
+  score: number;
+};
 
 const Page: React.FC = () => {
+  const apiUrl ='https://4609d7e8-cc66-4077-9781-04910e97ccb3-dev.e1-us-cdp-2.choreoapis.dev/dxxo/game-server-http/start-new-game-session-5c6/v1.0/api/gamesession'
+  // const apiUrl = 'http://localhost:8080/api/gamesession';
   const [resp, setResp] = useState<ServerGameEvent | null>(null);
-  const [socket, setSocket] = useState<WebSocket | null>(null);
-  const [timer, setTimer] = useState(5);
+  const [session, setSession] = useState<string>('');
+  const [timer, setTimer] = useState(60);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
@@ -26,70 +37,76 @@ const Page: React.FC = () => {
     return () => clearInterval(countdown);
   }, []);
 
-  useEffect(function mount() {
-    console.log('window.configs=' + window?.configs?.apiUrl ? window.configs.apiUrl : "/");
-  });
+  // useEffect(function mount() {
+  //   console.log('window.configs=' + window?.configs?.apiUrl ? window.configs.apiUrl : "/");
+  // });
 
   useEffect(() => {
-    // const socket = new WebSocket('ws://localhost:8080/streaming');
-    console.log('Trying to create a websocket for url =' + apiUrl)
-    const socket = new WebSocket(apiUrl);
-    setSocket(socket);
-    socket.addEventListener('open', () => {
-      console.log('WebSocket connection established');
-      // socket.send('Hello, server!');
-    });
-    socket.addEventListener('message', (event) => {
-          console.log('Response from server:', event.data);
-          event.data.arrayBuffer().then((arrayBuffer: ArrayBuffer) => {
-          const uint8Array = new Uint8Array(arrayBuffer);
-          const randomEvent = ServerGameEvent.fromBinary(uint8Array);
-          console.log('ServerGameEvent received: ', randomEvent);
-          if (randomEvent.score) {
-            setScore(randomEvent.score);
-          }
-          setResp(randomEvent);
-      });
-    });
-
-    socket.addEventListener('error', (event) => {
-      console.error('WebSocket error:', event);
+    const fetchSession = async () => {
+      let sessionId = 'empty';
+      try {
+        const response = await fetch(apiUrl+"/start", {
+          method: 'POST'
+        });
+        sessionId = await response.text();
+        console.log('session from server: ' + sessionId)
+        setSession(sessionId);
+      } catch (error) {
+        console.error('Error starting new session:', error);
+        setGameOver(true);
+        setErrorMessage('Match error');
+      }
+      return sessionId;
+    };
+  
+    const fetchEvents = async (session: string) => {
+    try {
+      console.log("call "+ apiUrl+"/next?session="+session);
+      const response = await fetch(apiUrl+"/next?session="+session);
+      const randomEvent: ServerGameEvent = await response.json(); // Get the response as an ArrayBuffer
+      console.log('ServerGameEvent received: ', randomEvent);
+      console.log(`ServerGameEvent x: ${randomEvent.x}, y: ${randomEvent.y}, score: ${randomEvent.score}, end: ${randomEvent.end}`);
+      if (randomEvent.score) {
+        setScore(randomEvent.score);
+      }
+      setResp(randomEvent);
+    } catch (error) {
+      console.error('Error fetching events:', error);
       setGameOver(true);
       setErrorMessage('Match error');
-    });
-
-    socket.addEventListener('close', (event) => {
-      console.log('WebSocket connection closed: ', event);
-      setGameOver(true);
-    });
-
-    return () => {
-      socket.close();
-    };
-  }, []); // Empty dependency array to run the effect only once
-  const handleClick = useRef<(x: number, y: number) => void>(() => {});
-
-useEffect(() => {
-  handleClick.current = (x: number, y: number) => {
-    if (socket) {
-      var pl = new PlayerClickedEvent();
-      pl.x = x;
-      pl.y = y;
-      const binaryData = (pl as Message<PlayerClickedEvent>).toBinary();
-      socket.send(binaryData);
     }
   };
-}, [socket]);
+  const runAsyncTasks = async () => {
+    const sessionId = await fetchSession();
+    // fetchEvents(sessionId);
+    const interval = setInterval(() => fetchEvents(sessionId), 1000);
+    return () => clearInterval(interval);
+  };
+  
+    runAsyncTasks();
+  }, []); // Empty dependency array to run effect only once
 
-useEffect(() => {
-  const button = document.getElementById('targetButton');
-  if (button && resp) {
-    button.addEventListener('click', () => handleClick.current(resp.targetX, resp.targetY));
-    return () => {
-      button.removeEventListener('click', () => handleClick.current);
-    };
-  }
-}, [resp]);
+
+  const handleClick = async (x: number, y: number) => {
+    try {
+      const pl: PlayerClickedEvent = { x, y };
+      const response = await fetch(apiUrl + "/click?session="+session, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(pl)
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to send click event');
+      }
+    } catch (error) {
+      console.error('Error sending click event:', error);
+      setGameOver(true);
+      setErrorMessage('Match error');
+    }
+  };
   
   return (
     <div>
@@ -121,9 +138,10 @@ useEffect(() => {
             id="targetButton"
             style={{
               position: 'absolute',
-              left: `${resp.targetX}px`,
-              top: `${resp.targetY}px`
+              left: `${resp.x}px`,
+              top: `${resp.y}px`
             }}
+            onClick={() => handleClick(resp.x, resp.y)}
           >
             CLICK ME
           </button>
